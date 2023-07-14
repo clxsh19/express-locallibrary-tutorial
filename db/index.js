251522196a3168401aa37eac844cdc69d01ea75c
clxsh19 <clxsh19@gmail.com>
2023-07-14 22:23:33 +0530

    corrected mistake in  db file

--- db/index.js
+++ db/index.js
@@ -1,19 +1,12 @@
 require('dotenv').config();
 const { Pool } = require('pg');
 
-// const dev_db = {
-//   user: 'postgres',
-//   host: 'localhost',
-//   database: 'library',
-//   password: '19122001',
-//   port: 5432,
-// }
-
 const pool = new Pool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_DATABASE,
+  port: process.env.DB_PORT,
 });
 
 const query = (text, params, callback) => {
